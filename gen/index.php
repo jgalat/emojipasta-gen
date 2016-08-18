64b7399a014c28031bd1340462abcac59a7d03bf
Jorge Galat <jrgglt@gmail.com>
2016-08-18 10:40:18 -0300

    Improved js to use jquery.

--- gen/index.php
+++ gen/index.php
@@ -16,6 +16,4 @@
     echo $emojipasta;
   }
 
-
-
 ?>
--- js/emojipasta.js
+++ js/emojipasta.js
@@ -1,47 +1,40 @@
 $.getScript("js/jMinEmoji.js");
 
 function emojipasta() {
-  var emojipasta = document.getElementById("emojipasta");
   setactivetab(0);
   $.get("gen/", function (data){
-      emojipasta.value = data;
+      $("#emojipasta").val(data);
     });
 };
 
 function copypasta(){
-  var emojipasta = document.getElementById("emojipasta");
   setactivetab(0);
-  emojipasta.select();
+  $("#emojipasta").select();
   document.execCommand("copy");
 };
 
 function setactivetab(n){
-  var emojipasta = document.getElementById("emojipasta");
-  var previewpasta = document.getElementById("previewpasta");
-  var text = document.getElementById("text");
-  var apple = document.getElementById("apple");
-  var android = document.getElementById("android");
   switch(n) {
     case 0:
-      previewpasta.style.display = "none";
-      emojipasta.style.display = "block";
-      text.classList.add("active");
-      apple.classList.remove("active");
-      android.classList.remove("active");
+      $("#previewpasta").css("display", "none");
+      $("#emojipasta").css("display", "block");
+      $("#text").addClass("active");
+      $("#apple").removeClass("active");
+      $("#android").removeClass("active");
       break;
     case 1:
-      emojipasta.style.display = "none";
-      previewpasta.style.display = "block";
-      apple.classList.add("active");
-      text.classList.remove("active");
-      android.classList.remove("active");
+      $("#emojipasta").css("display", "none");
+      $("#previewpasta").css("display", "block");
+      $("#apple").addClass("active");
+      $("#text").removeClass("active");
+      $("#android").removeClass("active");
       break;
     case 2:
-      emojipasta.style.display = "none";
-      previewpasta.style.display = "block";
-      android.classList.add("active");
-      text.classList.remove("active");
-      apple.classList.remove("active");
+      $("#emojipasta").css("display", "none");
+      $("#previewpasta").css("display", "block");
+      $("#android").addClass("active");
+      $("#text").removeClass("active");
+      $("#apple").removeClass("active");
       break;
     default:
       break;
@@ -49,19 +42,17 @@ function setactivetab(n){
 };
 
 function previewpasta(n){
-  var emojipasta = document.getElementById("emojipasta");
-  var previewpasta = document.getElementById("previewpasta");
-  var emojitext = emojipasta.value;
-  previewpasta.innerHTML = emojitext;
+  var emojitext = $("#emojipasta").val();
+  $("#previewpasta").html(emojitext);
   setactivetab(n);
   switch(n) {
     case 0:
       break;
     case 1:
-      $('.preview').minEmoji("apple");
+      $(".preview").minEmoji("apple");
       break;
     case 2:
-      $('.preview').minEmoji("android");
+      $(".preview").minEmoji("android");
       break;
     default:
       break;
